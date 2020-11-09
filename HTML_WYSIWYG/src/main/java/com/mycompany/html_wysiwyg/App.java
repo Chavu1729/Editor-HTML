2dcac1a9c19fc5c1350952ef5284284933fdb8a0
javir <javir@DESKTOP-6VLDSBU>
2020-11-09 20:16:22 +0100

    Final project revision.

--- HTML_WYSIWYG/src/main/java/com/mycompany/html_wysiwyg/App.java
+++ HTML_WYSIWYG/src/main/java/com/mycompany/html_wysiwyg/App.java
@@ -16,25 +16,47 @@ public class App extends Application {
 
     private static Scene scene;
 
+    /**
+     *
+     * @param stage
+     * @throws IOException
+     */
     @Override
     public void start(Stage stage) throws IOException {
         Image image = new Image("images/html_index.png");
-        scene = new Scene(loadFXML("primary"), 900, 700);
+        scene = new Scene(loadFXML("primary"), 900, 750);
+        stage.setMinWidth(900);
+        stage.setMinHeight(750);
         stage.setTitle("HTML Editor");
         stage.setScene(scene);
         stage.getIcons().add(image);
         stage.show();
     }
 
+    /**
+     * 
+     * @param fxml
+     * @throws IOException 
+     */
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
+    /**
+     * 
+     * @param fxml
+     * @return
+     * @throws IOException 
+     */
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         launch();
     }
