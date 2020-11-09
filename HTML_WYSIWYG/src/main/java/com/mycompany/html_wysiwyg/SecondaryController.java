1d2aaba1ddd5385ebbf314f8fd4a2fabeb9197ea
javir <javir@DESKTOP-6VLDSBU>
2020-11-09 20:14:13 +0100

    Final project revision.

--- HTML_WYSIWYG/src/main/java/com/mycompany/html_wysiwyg/SecondaryController.java
+++ HTML_WYSIWYG/src/main/java/com/mycompany/html_wysiwyg/SecondaryController.java
@@ -4,14 +4,23 @@ import javafx.fxml.FXML;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
+/**
+ *
+ * @author Javier Torres Sevilla, Cristina Domenech Moreno
+ */
 public class SecondaryController{
 
     @FXML
     private WebView webView;
 
+    /**
+     * This method obtains the HTML text from the editor area of the previous controller and sends it to the web view for displaying.
+     * @param htmlText String that will be sent to web view to show.
+     */
     public void setHTML(String htmlText) {
         final WebEngine mobileEngine = webView.getEngine();
         
+        // This sets the HTML content to be non-editable for the mobile view.
         if (htmlText.contains("contenteditable=\"true\"")) {
             htmlText = htmlText.replace("contenteditable=\"true\"", "contenteditable=\"false\"");
         }
